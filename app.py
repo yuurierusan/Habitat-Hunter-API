1a56eeec2cdd9bfbbaeca32d1a30c9b482b5b51d
Jonshea Nutson <yuurieru@icloud.com>
2023-03-26 17:14:17 -0400

    created post routes for api

--- app.py
+++ app.py
@@ -1,10 +1,68 @@
-from dotenv import load_dotenv, find_dotenv
-from pymongo import MongoClient
-import pprint
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo, ObjectId
+from dotenv import load_dotenv
+from flask_cors import CORS
+import datetime
+
 import os
 
-load_dotenv(find_dotenv())
+load_dotenv()
 
+APP_ENV = os.environ.get('APP_ENV')
+DEBUG = os.environ.get('DEBUG')
 MONGO_URI = os.environ.get('MONGO_URI')
 
-client = MongoClient(MONGO_URI)
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = MONGO_URI
+mongo = PyMongo(app)
+
+CORS(app)
+
+db = mongo.db.HabitatHunter
+
+
+@app.route("/users", methods=['POST'])
+def createUser():
+    id = db.insert({
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'username': request.json['username']
+    })
+    return jsonify({'id': str(ObjectId(id)), 'msg': 'User created successfully!'})
+
+
+@app.route("/owners", methods=['POST'])
+def createOwner():
+    id = db.insert({
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'username': request.json['username']
+    })
+    return jsonify({'id': str(ObjectId(id)), 'msg': 'Owner created successfully!'})
+
+
+@app.route("/listings", methods=['POST'])
+def createListing():
+    id = db.insert({
+        'image': request.json['image'],
+        'title': request.json['title'],
+        'price': request.json['price']
+        'date': datetime.datetime.fromtimestamp(request.json['date'])
+        'amenities': request.json['amenities']
+    })
+    return jsonify({'id': str(ObjectId(id)), 'msg': 'Listing added successfully!'})
+
+
+@app.route("/comments", methods=['POST'])
+def createComment():
+    id = db.insert({
+        'username': request.json['username'],
+        'content': request.json['content'],
+        'date': datetime.datetime.fromtimestamp(request.json['date'])
+    })
+    return jsonify({'id': str(ObjectId(id)), 'msg': 'Comment posted successfully!'})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
