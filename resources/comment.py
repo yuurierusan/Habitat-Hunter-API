c622a750ec832e9b016e08870b36951c0067a8c7
Jonshea Nutson <yuurieru@icloud.com>
2023-03-28 10:02:19 -0400

    updated resources for embeded docs

--- resources/comment.py
+++ resources/comment.py
@@ -1,3 +1,23 @@
-from flask import request, session, jsonify, make_response
+from flask import jsonify, make_response
 from flask_restful import Resource
 from models.comment import Comment
+
+
+def index():
+    if not session.get("email"):
+        return False
+    return True
+
+
+class Comments(Resource):
+    def get(self):
+        comments = Comment.objects()
+        return make_response(jsonify(comments), 200)
+
+    def get_comment_by_id(id: str):
+        if index():
+            comment = Comment.objects(comments=current_comment).first()
+            id = Comment.objects(id=id)
+            if comment and id:
+                return jsonify(id), 200
+            return {'msg': 'Unable to find comment'}, 404
--- resources/listing.py
+++ resources/listing.py
@@ -1,3 +1,23 @@
-from flask import request, session, jsonify, make_response
+from flask import jsonify, make_response
 from flask_restful import Resource
 from models.listing import Listing
+
+
+def index():
+    if not session.get("email"):
+        return False
+    return True
+
+
+class Listings(Resource):
+    def get(self):
+        listings = Listing.objects()
+        return make_response(jsonify(listings), 200)
+
+    def get_listing_by_id(id: str):
+        if index():
+            listing = Listing.objects(listing=current_listing).first()
+            id = Listing.objects(id=id)
+            if listing and id:
+                return jsonify(id), 200
+            return {'msg': 'Unable to find listing'}, 404
