f4b6531f83d1d40c2b17c697609a29a4d103a8e4
Jonshea Nutson <yuurieru@icloud.com>
2023-03-30 15:38:08 -0400

    get comment and create comment work

--- resources/comment.py
+++ resources/comment.py
@@ -3,6 +3,7 @@ from flask import request, session, jsonify, make_response
 from flask_restful import Resource
 from models.user import User
 from models.comment import Comment
+from models.db import db
 
 
 def index():
@@ -13,27 +14,31 @@ def index():
 
 class Comments(Resource):
     def get(self):
-        comments = Comment.objects()
-        return make_response(jsonify(comments), 200)
+        users = User.objects()
+        comments = []
+        for user in users:
+            comments.extend(user.comments)
+        return make_response(jsonify(user.name, comments), 200)
 
     def get_comment_by_id(id: str):
-        if index():
-            comment = Comment.objects(name=current_comment).first()
-            id = Comment.objects(id=id)
-            if comment and id:
-                return jsonify(id), 200
-            return {'msg': 'Unable to find comment'}, 404
+        user = User.objects(id=id).first()
+        comment = []
+        if id:
+            comment.extend(user.comments)
+            return make_response(jsonify(user.id, comments), 200)
+        return {'msg': 'Unable to find comment'}, 404
 
 
 class NewComment(Resource):
     @jwt_required()
-    def post(self):
+    def put(self):
         current_user = get_jwt_identity()
         user = User.objects(email=current_user).first()
         if user:
             comment = Comment()
             body = request.get_json()
-            comment.title = body.get("title")
-            comment.content = body.get("content")
-            comment.save()
-            return {"message": "Posted comment"}, 200
+            comment.title = body.get("comments.title")
+            comment.content = body.get("comments.content")
+            user.comments.append(comment)
+            user.update(**body)
+            return {"message": "Updated user comments"}, 200
