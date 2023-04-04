9a7e1530dfa9165b1a0d4056e0fd5e7f2a8430a5
Jonshea Nutson <yuurieru@icloud.com>
2023-04-04 01:54:08 -0400

    added id to models

--- models/comment.py
+++ models/comment.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from models.db import db
+import uuid
 
 
 class Comment(db.EmbeddedDocument):
+    id = db.StringField(primary_key=True, default=lambda: str(uuid.uuid4()))
     title = db.StringField()
     content = db.StringField()
     creation_date = db.DateTimeField(default=datetime.now)
--- models/listing.py
+++ models/listing.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from models.db import db
+import uuid
 
 
 class Listing(db.EmbeddedDocument):
+    id = db.StringField(primary_key=True, default=lambda: str(uuid.uuid4()))
     image = db.StringField()
-    title = db.StringField(unique=True)
+    title = db.StringField()
     content = db.StringField()
     price = db.IntField()
     type = db.StringField()
