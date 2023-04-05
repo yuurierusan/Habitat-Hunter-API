5d87565e628ee0c283f7f86cb22fa37b41dc1a5f
Jonshea Nutson <yuurieru@icloud.com>
2023-04-05 18:15:38 -0400

    changed listing model to accept URLs

--- models/listing.py
+++ models/listing.py
@@ -5,12 +5,10 @@ import uuid
 
 class Listing(db.EmbeddedDocument):
     id = db.StringField(primary_key=True, default=lambda: str(uuid.uuid4()))
-    image = db.StringField()
+    image = db.URLField()
     title = db.StringField()
     content = db.StringField()
     price = db.IntField()
-    type = db.StringField()
-    icon = db.StringField()
     amenities = db.StringField()
     creation_date = db.DateTimeField(default=datetime.now)
     modified_date = db.DateTimeField(default=datetime.now)
