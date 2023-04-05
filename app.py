13d613e26bc50d5523a20bf622e4ae1e6f79ff87
Jonshea Nutson <yuurieru@icloud.com>
2023-04-05 17:34:13 -0400

    app is currently working as intended

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ api.add_resource(CheckSession, '/session')
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=8080)
--- resources/listing.py
+++ resources/listing.py
@@ -46,9 +46,7 @@ class NewListing(Resource):
             listing.image = body.get("image")
             listing.price = body.get("price")
             listing.content = body.get("content")
-            listing.icon = body.get("icon")
             listing.amenities = body.get("amenities")
-            listing.type = body.get("type")
             user.listings.append(listing)
             user.save()
             return {"message": f"Updated {user.name} listings"}, 200
@@ -64,11 +62,9 @@ class UpdateListing(Resource):
             for listing in user.listings:
                 if listing.id == id:
                     body = request.get_json()
-                    # listing.image = request.json.get('image', listing.image)
+                    listing.image = request.json.get('image', listing.image)
                     listing.title = request.json.get('title', listing.title)
-                    # listing.icon = request.json.get('icon', listing.icon)
                     listing.price = request.json.get('price', listing.price)
-                    # listing.type = request.json.get("type", listing.type)
                     listing.content = request.json.get(
                         'content', listing.content)
                     listing.amenities = request.json.get(
